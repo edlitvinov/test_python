e5b84fcd6665c162bdd551d40bca186621ef2df6
edlitvinov <edoss@list.ru>
2022-07-04 19:54:34 +0400

    1

---
+++ test.py
@@ -0,0 +1,33 @@
+import turtle
+
+# turtle.color("black", "red")
+# turtle.begin_fill()
+# for i in range(0, 4):
+#     turtle.forward(50)
+#     turtle.right(90)
+# turtle.penup()
+# turtle.end_fill()
+# turtle.forward(100)
+# turtle.pendown()
+
+# turtle.color("black", "green")
+# turtle.begin_fill()
+# for i in range(0, 4):
+#     turtle.forward(50)
+#     turtle.right(90)
+# turtle.penup()
+# turtle.end_fill()
+# turtle.forward(100)
+# turtle.pendown()
+
+# turtle.color("black", "yellow")
+# turtle.begin_fill()
+# for i in range(0, 4):
+#     turtle.forward(50)
+#     turtle.right(90)
+# turtle.penup()
+# turtle.end_fill()
+# turtle.forward(100)
+# turtle.pendown()
+
+# turtle.exitonclick()
